558a3e7d1faf980962669567cb648a1bcd99d7b3
Agna Desai <Agna.Desai@agenabio.com>
2017-11-22 12:50:40 -0800

    Example project to show the proxy error for Springboot with spring loaded

---
+++ src/main/java/com/myproject/HomeController.java
@@ -0,0 +1,45 @@
+package com.myproject;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Davor Sauer
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @RequestMapping(value = "/time", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public LocalDateTime time() {
+        return LocalDateTime.now();
+    }
+
+    @RequestMapping(value = "/name/{name}")
+    @ResponseBody
+    public String name(@PathVariable("name") String name) {
+        return "Hi there: " + name;
+    }
+
+    /**
+     * Add following method after classes -t task and after bootrun task to see if spring loaded shows the new endpoint
+     * You should see error  org.springsource.loaded.ReloadableType   : Unable to find regeneration methods for cglib proxies - proxies will be out of date for this type
+     */
+    @RequestMapping(value = "/afterBootRun", produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public String helloWorldAfterRunStarts() {
+        return "Hello World after BootRun with SpringLoaded!";
+    }
+
+}
